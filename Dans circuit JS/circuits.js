38349aaa6756aa0bc93fd3b32f76ac54de970837
DanBlackwell <danblackwell95@yahoo.co.uk>
2016-01-12 17:30:05 +0000

    Maths working AFAIC

--- Dans circuit JS/circuits.js
+++ Dans circuit JS/circuits.js
@@ -8,26 +8,46 @@
 // 	parallelDepth : 1
 // }
 
-var componentArray = [{id:0, type:"cell", voltage:9, resistance:0, nextComponent:[1], previousComponent:[2,3], parallelDepth:null}, //cell
-	{id:1, type:"resistor1", voltage:0, resistance:1, nextComponent:[2,3], previousComponent:[0], parallelDepth:null},
-	{id:2, type:"resistor2", voltage:0, resistance:1, nextComponent:[0], previousComponent:[1], parallelDepth:null},
-	{id:3, type:"resistor3", voltage:0, resistance:1, nextComponent:[0], previousComponent:[1], parallelDepth:null},
+var componentArray = [{id:0, type:"cell", voltage:9, resistance:0, current:0, nextComponent:[1], previousComponent:[2,3], parallelDepth:null}, //cell
+	{id:1, type:"resistor1", voltage:0, resistance:1, current:0, nextComponent:[2,3], previousComponent:[0], parallelDepth:null},
+	{id:2, type:"resistor2", voltage:0, resistance:1, current:0, nextComponent:[0], previousComponent:[1], parallelDepth:null},
+	{id:3, type:"resistor3", voltage:0, resistance:1, current:0, nextComponent:[0], previousComponent:[1], parallelDepth:null},
 ]; //this will need a tidier function to sort objects into a list order to make them selectable and change the next and previous components to suit, eg if someone creates a load of components then deletes them it will throw the ids out of sync with the array index.
 
 calcParallelDepths();
 calcTotalVoltage();
 calcTotalResistance();
 addVoltageDrops();
+calcCurrents();
+printInfo();
 
-// var component = {
-// 	id : 1
-// 	Type : "resistor"
-// 	Voltage : 1
-// 	Resistance : 1
-// 	Nextcomponent : [2,3]
-// 	previousComponent : [0]
-// 	parallelDepth : 1
-// }
+function printInfo () {
+	var component;
+	process.stdout.write("type:" + '\t');
+	process.stdout.write("voltage" + '\t');
+	process.stdout.write("current" + '\t');
+	process.stdout.write("resistance" + '\n');
+	for (i = 0; i < componentArray.length; i++) {
+		component = componentArray[i];
+		// console.log(JSON.stringify(component));
+		process.stdout.write(component.type + '\t');
+		process.stdout.write(component.voltage + '\t');
+		process.stdout.write(component.current + '\t');
+		process.stdout.write(component.resistance + '\n');
+	}
+}
+
+function calcCurrents() {
+	var component;
+	for (i = 0; i < componentArray.length; i++) {
+		component = componentArray[i];
+		if (component.voltage < 0 && component.resistance > 0) {
+			component.current = -component.voltage / component.resistance;
+		} else {
+			component.current = 0;
+		}
+	}
+}
 
 function calcParallelDepths() { //start at component 0 and figure recursively
 	var startComp = 0;
@@ -47,7 +67,7 @@ function calcParallelDepths() { //start at component 0 and figure recursively
 	} while(component.id != 0);
 }
 
-function calcParallelDepthsHelper(depth, component) { 
+function calcParallelDepthsHelper(depth, component) {
 	var origComponent = component;
 	for (i = 0; i < origComponent.nextComponent.length; i++) {
 		component = componentArray[origComponent.nextComponent[i]];
@@ -61,12 +81,12 @@ function calcParallelDepthsHelper(depth, component) {
 					component = componentArray[component.nextComponent[0]]; //move past this parallel loop til the next individual component
 				} while (component.parallelDepth == depth + 1)
 			} else {
-				component = componentArray[component.nextComponent[0]]; 
+				component = componentArray[component.nextComponent[0]];
 				if (component.previousComponent.length > 1 && i == origComponent.nextComponent.length - 1) {
 					return;
 				}
 			}
-		} 
+		}
 	}
 }
 
@@ -84,15 +104,17 @@ function calcTotalVoltage() {
 function addVoltageDrops() {
 	var voltage = calcTotalVoltage();
 	while (component.parallelDepth == 0) {
-		component.voltage = voltage * component.resistance / findBranchResHelper(component); // set the voltage for the component you're on
+		component.voltage = -voltage * component.resistance / findBranchResHelper(component); // set the voltage for the component you're on
 		console.log("set component: %s voltage to : %j", component.type, component.voltage);
 		if (component.nextComponent.length > 1) {
 			console.log("component = %s", component.type);
 			var initalComp = component;
-			for (i = 0; i < component.nextComponent.length; i++) {
+			var parallelRes = findParallelCircuitResistance(component);
+			var voltageToShare = voltage * parallelRes / calcTotalResistance();
+			for (i = 0; i < initalComp.nextComponent.length; i++) {
 				component = componentArray[initalComp.nextComponent[i]];
-				console.log("L94 component = %s", component.type);
-				addVoltageDropsHelper((voltage * findParallelCircuitResistance(component) / findBranchResHelper(component)), component);
+				console.log("L94 component = %s, voltage : %j, branchTotal: %j\n\n", component.type, voltageToShare, findBranchResHelper(component));
+				addVoltageDropsHelper(voltageToShare, component);
 			}
 			while (component.parallelDepth > 0) {
 				component = componentArray[component.nextComponent[0]];
@@ -104,23 +126,25 @@ function addVoltageDrops() {
 			break;
 		}
 	}
-	for (i = 0; i < componentArray.length; i++) {
-		console.log("component: %s, volatge: %j", componentArray[i].type, componentArray[i].voltage); 
-	}
+	// for (i = 0; i < componentArray.length; i++) {
+	// 	console.log("component: %s, volatge: %j", componentArray[i].type, componentArray[i].voltage);
+	// }
 }
 
 
 function addVoltageDropsHelper(voltageAvail, component) {
-	console.log("entering addVoltageDropsHelper : volt %j, component %s", voltageAvail, component.type);
 	var branchResistance = findBranchResHelper(component);
+	console.log("entering addVoltageDropsHelper : volt %j, component %s, branchRes : %j", voltageAvail, component.type, branchResistance);
 	var initialDepth = component.parallelDepth;
 	while (component.parallelDepth == initialDepth) {
-		component.voltage = voltageAvail * component.resistance / branchResistance; // set the voltage for the component you're on
+		component.voltage = -voltageAvail * component.resistance / branchResistance; // set the voltage for the component you're on
 		if (component.nextComponent.length > 1) {
 			var initalComp = component;
-			for (i = 0; i < component.nextComponent.length; i++) {
+			var parallelRes = findParallelCircuitResistance(component);
+			var voltageToShare = voltage * parallelRes / calcTotalResistance();
+			for (i = 0; i < initalComp.nextComponent.length; i++) {
 				component = componentArray[initalComp.nextComponent[i]];
-				addVoltageDropsHelper(voltageAvail * findParallelCircuitResistance(component) / findBranchResHelper(component), component);
+				addVoltageDropsHelper(voltageToShare, component);
 			}
 			while (component.parallelDepth > initialDepth) {
 				component = componentArray[component.nextComponent[0]];
@@ -167,6 +191,7 @@ function findParallelCircuitResistance(component) {
 function findBranchResHelper(component) {
 	var originalDepth = component.parallelDepth;
 	var total = 0;
+	console.log("entering findBranchResHelper, component : %s", component.type);
 	do {
 		total += component.resistance;
 		if (component.nextComponent.length > 1) {
